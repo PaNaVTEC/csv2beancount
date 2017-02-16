7ab2ddf00c6389017fdd14b315dcbb0d70098820
Christian Panadero <panavtec@gmail.com>
2017-02-16 21:18:52 +0000

    Add code coverage tool

--- project.clj
+++ project.clj
@@ -10,4 +10,5 @@
                  [io.forward/yaml "1.0.5"]
                  [clj-time "0.13.0"]]
   :main     csv2beancount.core
-  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.2.1"]
+                             [lein-cloverage "1.0.9"]]}})
