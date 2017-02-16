b8003815b82f7a7cd43dd18f9851c16a189a38c2
Christian Panadero <panavtec@gmail.com>
2017-02-16 19:06:06 +0000

    Remove unnecesary function to get amounts

--- src/csv2beancount/transaction.clj
+++ src/csv2beancount/transaction.clj
@@ -3,32 +3,27 @@
 
 (defn- negative[x] (str "-" (str/trim x)))
 
-(defn- associated-amounts 
-  ([amount_in amount_out]
+(defn- toggle-sign[x] 
+  (let [amount (str/trim x)
+        is-negative (str/starts-with? amount "-")]
+    (if is-negative (subs amount 1) (str "-" amount))))
+
+(defn- associated-amounts [amount-in amount-out]
    (cond
-     (str/blank? amount_in) {:amount1 (negative amount_out) :amount2 amount_out}
-     (str/blank? amount_out) {:amount1 amount_in :amount2 (negative amount_in)}))
-  ([amount] 
-   (let [trimmed-amount (str/trim amount)
-         is-negative (str/starts-with? trimmed-amount "-")
-         amount2 (if is-negative (subs amount 1) (str "-" trimmed-amount))]
-     {:amount1 amount :amount2 amount2})))
+     (= amount-in amount-out) {:amount1 amount-in :amount2 (toggle-sign amount-in)}
+     (str/blank? amount-in) {:amount1 (negative amount-out) :amount2 amount-out}
+     (str/blank? amount-out) {:amount1 amount-in :amount2 (negative amount-in)}))
 
 (defn- associated-account [description rules default-account]
   (let [filtered-rules (filter #(.contains description (key %)) rules)
         rule (if (not-empty filtered-rules) (val (first filtered-rules)))]
     (get rule "account" default-account)))
 
-(defn- get-amounts[amount-in amount-out]
-  (if (= amount-in amount-out)
-    (associated-amounts amount-in)
-    (associated-amounts amount-in amount-out)))
-
 (defn get-transaction[rules row]
   (let [desc (get row (:desc-index rules))
         {:keys [amount1 amount2]} 
-        (get-amounts (get row (:amount-in-index rules))
-                     (get row (:amount-out-index rules)))]
+        (associated-amounts (get row (:amount-in-index rules))
+                            (get row (:amount-out-index rules)))]
     {:date (get row (:date-index rules))
      :desc desc
      :currency (:currency rules)
