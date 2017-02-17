fa2212465951c5772427a0e135d939f8f1e9653a
Christian Panadero <panavtec@gmail.com>
2017-02-17 17:32:39 +0000

    bump to 1.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv2beancount "1.0.4"
+(defproject csv2beancount "1.0.5"
   :description "A csv to beancount importer"
   :url "http://panavtec.me/"
   :license {:name "Eclipse Public License"
