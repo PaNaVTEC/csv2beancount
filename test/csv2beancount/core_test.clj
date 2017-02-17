8338d8aef93ecdaf4521cb8581af7eba057c1ede
Christian Panadero <panavtec@gmail.com>
2017-02-17 13:29:53 +0000

    refactor core test

--- test/csv2beancount/core_test.clj
+++ test/csv2beancount/core_test.clj
@@ -8,13 +8,15 @@
                       "  -y, --yaml Yaml  Yaml Path\n"
                       "  -h, --help\n"))
 
+(defn abs-path-of[path] (-> path io/resource io/file .getAbsolutePath))
+
 (deftest main-prints-usage-is-invoked-without-arguments
   (let [console-output (with-out-str (-main ""))]
     (is (= console-output help-output))))
 
 (deftest non-existing-file-should-output-that
   (let [non-existing-file "whatever_non_existing_file.txt"
-        params {:options {:csv non-existing-file :yaml non-existing-file} :summary help-output }
+        params {:options {:csv non-existing-file :yaml non-existing-file}}
         console-output (with-out-str (run-program params))]
     (is (= console-output "The file provided in --csv does not exist\n"))))
 
@@ -23,8 +25,8 @@
                              "  Income:UK:Codurance:Salary -5000.00 GBP\n\n"))
 
 (deftest single-line-csv-parses-a-transaction
-  (let [csv-path (-> "single_line_transaction.csv" io/resource io/file .getAbsolutePath)
-        yml-path (-> "simple_transaction_rules.yaml" io/resource io/file .getAbsolutePath)
+  (let [csv-path (abs-path-of "single_line_transaction.csv")
+        yml-path (abs-path-of "simple_transaction_rules.yaml")
         params {:options {:csv csv-path :yaml yml-path} :summary help-output }
         console-output (with-out-str (run-program params))]
     (is (= console-output single-transaction))))
@@ -34,15 +36,16 @@
                                       "  Expenses:Unknown -52.00 GBP\n\n"))
 
 (deftest default-account-when-no-rules-match
-  (let [csv-path (-> "transaction_not_in_rules.csv" io/resource io/file .getAbsolutePath)
-        yml-path (-> "simple_transaction_rules.yaml" io/resource io/file .getAbsolutePath)
+  (let [csv-path (abs-path-of "transaction_not_in_rules.csv")
+        yml-path (abs-path-of "simple_transaction_rules.yaml")
         params {:options {:csv csv-path :yaml yml-path} :summary help-output }
         console-output (with-out-str (run-program params))]
     (is (= console-output default-account-transaction))))
 
 (deftest default-account-when-no-rules-match
-  (let [csv-path (-> "transaction_with_alternative_delimiter.csv" io/resource io/file .getAbsolutePath)
-        yml-path (-> "alternative_delimiter_rules.yaml" io/resource io/file .getAbsolutePath)
+  (let [csv-path (abs-path-of "transaction_with_alternative_delimiter.csv")
+        yml-path (abs-path-of "alternative_delimiter_rules.yaml")
         params {:options {:csv csv-path :yaml yml-path} :summary help-output }
         console-output (with-out-str (run-program params))]
     (is (= console-output default-account-transaction))))
+
